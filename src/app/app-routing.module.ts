12e847f73baee8875ca477a20692212636477f3b
BenzoL17 <sergeev.nickitos@yandex.ru>
2018-04-29 18:21:16 +0300

    code syle refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,39 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ScheduleComponent } from './components/schedule/schedule.component';
 import { StudentSelectorComponent } from './components/student-selector/student-selector.component';
 import { SubjectSelectorComponent } from './components/subject-selector/subject-selector.component';
 
+
 const routes: Routes = [
-  {
-    path: 'schedule',
-    component: ScheduleComponent
-  },
-  {
-    path: 'marks',
-    component: StudentSelectorComponent
-  },
-  {
-    path: 'marks/submit',
-    component: SubjectSelectorComponent
-  },
-  {
-    path: '',
-    redirectTo: '/schedule',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    redirectTo: '/schedule',
-    pathMatch: 'full'
-  }
+    {
+        path: 'schedule',
+        component: ScheduleComponent
+    },
+    {
+        path: 'marks',
+        component: StudentSelectorComponent
+    },
+    {
+        path: 'marks/submit',
+        component: SubjectSelectorComponent
+    },
+    {
+        path: '',
+        redirectTo: '/schedule',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/schedule',
+        pathMatch: 'full'
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
+
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -13,7 +15,7 @@ describe('AppComponent', () => {
         }).compileComponents();
     }));
 
-    test('should create the app', async(() => {
+    it('should create the app', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeDefined();
